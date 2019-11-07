130460d407203c09ebfc807495c207521c22ebec
tkskds <infourbh+1@gmail.com>
2019-11-07 14:15:47 +0900

    fix

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 ">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <?php get_template_part('parts/meta') ?>
     <?php wp_head(); ?>
   </head>
 
