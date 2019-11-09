1274ad424593fcb73b2e18064706ec88346557db
tkskds <infourbh+1@gmail.com>
2019-11-09 10:29:01 +0900

    test: bootstrap

--- parts/head/link.php
+++ parts/head/link.php
@@ -1,9 +1,17 @@
-<!--スタイルシートの呼び出し-->
+<?php //スタイルシート呼び出し ?>
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-<link rel="stylesheet" href="<?php echo get_template_part('lib/css/components'); ?>">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/lib/css/components.css">
+
+
+
+<?php //サードパーティ製css/js呼び出し ?>
+<!--TEST-->
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"><!--font-awesomeのスタイルシートの呼び出し-->
 
-<!--アイコン-->
+
+
+<?php //ファビコンなど ?>
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
 <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon.png">
 <link rel="icon" type="image/png" size="256x256" href="<?php echo get_template_directory_uri(); ?>/images/android-chrome.png">
