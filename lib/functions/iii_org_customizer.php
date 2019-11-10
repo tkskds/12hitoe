b689a2c9cdc95b7f27b6482540c8e2acd6635733
tkskds <infourbh+1@gmail.com>
2019-11-10 12:28:07 +0900

    add: customizer

--- lib/functions/iii_org_customizer.php
+++ lib/functions/iii_org_customizer.php
@@ -1,15 +1,76 @@
 <?php
 
+
+// MEMO
+//
+// ## 大見出し
+// $wp_customize->add_panel('変数', array(
+//     'priority' => 1,
+//     'title' => 'サイトの基本設定',
+// ));
+//
+// ## 小見出し
+// $wp_customizer->add_section('変数', array(
+//
+// ));
+//
+// ## 項目
+// $wp_customizer->add_settings('変数', array(
+//
+// ));
+//
+// ## 設定
+// $wp_customizer->add_controle('変数',array(
+//
+// ));
+
+
+
 //////////////////////////////
 // 3 カスタマイザー項目追加
 //////////////////////////////
 
-function org_customizer(){
+function org_customizer($wp_customizer){
 
-  //TEST
-  $wp_cus->add_settings('test[test]', array(
-    'default' => '',
-    'type' => 'option',
-    'trasnport' =>  'postMassage',
+  // STEP1
+  $wp_customizer->add_panel('site_conf', array(
+    'priority' => 1,
+    'title' => 'STEP1【基本設定】',
+    'description' => 'サイトの基本設定'
   ));
+
+      $wp_customizer->add_section('title_tagline', array(
+        'title' => '基本情報とロゴの設定',
+        'panel' => 'site_conf',
+      ));
+
+      $wp_customizer->add_section('meta_description', array(
+        'title' => 'サイトの説明文',
+
+      ));
+
+      $wp_customizer->add_setting('meta_description', array(
+        'default' => '',
+        'type' => 'option',
+        'transport'  => 'postMessage',
+      ));
+
+
+
+  // STEP2
+  $wp_customizer->add_panel('site_builder', array(
+    'priority' => 2,
+    'title' => 'STEP2【外観設定】',
+    'description' => 'サイトの外観設定',
+  ));
+
+
+  // STEP3
+
+  $wp_customizer->add_panel('site_admin', array(
+    'priority' => 3,
+    'title' => 'STEP3【運営設定】',
+    'description' => 'サイトの運営設定',
+  ));
+
 } //END org_customizer()
