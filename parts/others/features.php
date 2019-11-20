c7463f3d626e193e62feefc03d35fbceeb145da2
tkskds <infourbh+1@gmail.com>
2019-11-20 11:12:21 +0900

    fix: QandA

--- parts/others/features.php
+++ parts/others/features.php
@@ -95,14 +95,49 @@
   <?php if($sec4q1 != null): ?>
     <section class="feature4">
       <div class="container featureContainer">
-        <div class="feature_qa">
-          <div class="feature_actab">
+        <div class="feature_actab">
           <input id="feature_tabfour1" type="checkbox" name="tabs">
           <label for="feature_tabfour1"><?php echo $sec4q1 ?></label>
           	<div class="feature_actab-content">
           	<p><?php echo $sec4a1 ?></p>
           	</div>
         </div>
+        <?php if($sec4q2 != null): ?>
+          <div class="feature_actab">
+            <input id="feature_tabfour2" type="checkbox" name="tabs">
+            <label for="feature_tabfour2"><?php echo $sec4q2 ?></label>
+              <div class="feature_actab-content">
+              <p><?php echo $sec4a2 ?></p>
+              </div>
+          </div>
+        <?php endif; ?>
+        <?php if($sec4q3 != null): ?>
+          <div class="feature_actab">
+            <input id="feature_tabfour3" type="checkbox" name="tabs">
+            <label for="feature_tabfour3"><?php echo $sec4q3 ?></label>
+              <div class="feature_actab-content">
+              <p><?php echo $sec4a3 ?></p>
+              </div>
+          </div>
+        <?php endif; ?>
+        <?php if($sec4q4 != null): ?>
+          <div class="feature_actab">
+            <input id="feature_tabfour4" type="checkbox" name="tabs">
+            <label for="feature_tabfour4"><?php echo $sec4q4 ?></label>
+              <div class="feature_actab-content">
+              <p><?php echo $sec4a4 ?></p>
+              </div>
+          </div>
+        <?php endif; ?>
+        <?php if($sec4q5 != null): ?>
+          <div class="feature_actab">
+            <input id="feature_tabfour5" type="checkbox" name="tabs">
+            <label for="feature_tabfour5"><?php echo $sec4q5 ?></label>
+              <div class="feature_actab-content">
+              <p><?php echo $sec4a5 ?></p>
+              </div>
+          </div>
+        <?php endif; ?>
       </div>
     </section>
   <?php endif; ?>
