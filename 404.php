554b04ceac61153b1b131a51bb382e095d085d91
tkskds <infourbh+1@gmail.com>
2019-11-28 10:26:18 +0900

    add: Error

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+  <div class="row contentArea">
+    <main id="main"
+          class="<?php
+                    //サイト構造がサイドバーを含むものならグリッドクラス付与
+                    $sideOn = get_option('site_bone_type');
+                    if ($sideOn == 'value1' || $sideOn == 'value3'){
+                      echo 'col s12 l9';
+                      }
+                  ?> main">
+      <div class="main__container">
+        <?php get_template_part('parts/article/not_found'); ?>
+      </div>
+    </main>
+    <?php if ($sideOn == 'value1' || $sideOn == 'value3'){ get_sidebar(); } ?>
+  </div>
+<?php get_footer(); ?>
---
+++ parts/article/not_found.php
@@ -0,0 +1,16 @@
+<article class="notFound">
+  <div class="article_container">
+    <!--タイトル-->
+    <div class="article_title">
+        <h1>記事が見つかりませんでした</h1>
+    </div>
+    <!--アイキャッチ-->
+    <div class="article_thumbnail">
+      <img src="<?php echo get_template_directory_uri()?>/images/notFound.png" alt="NotFound">
+    </div>
+    <!--本文-->
+    <div class="article_content">
+      <p>記事が見つかりませんでした。</p>
+    </div>
+  </div>
+</article>
