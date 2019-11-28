66949c80abd26f2924ac8d142edc09c958f202a2
tkskds <infourbh+1@gmail.com>
2019-11-28 10:52:26 +0900

    add: notFoundImg

--- parts/article/not_found.php
+++ parts/article/not_found.php
@@ -6,7 +6,7 @@
     </div>
     <!--アイキャッチ-->
     <div class="article_thumbnail">
-      <img src="<?php echo get_template_directory_uri()?>/images/notFound.png" alt="NotFound">
+      <img src="<?php echo get_template_directory_uri()?>/images/NotFound/<?php echo rand(1,5)?>.png" alt="NotFound">
     </div>
     <!--本文-->
     <div class="article_content">
