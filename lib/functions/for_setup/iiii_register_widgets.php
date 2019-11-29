11c527de69dd7d690943d286cdbfe7b77e8f08e4
tkskds <infourbh+1@gmail.com>
2019-11-29 11:30:50 +0900

    add: footerWidget

--- lib/functions/for_setup/iiii_register_widgets.php
+++ lib/functions/for_setup/iiii_register_widgets.php
@@ -12,9 +12,50 @@ function register_widgets(){
   register_sidebar(array(
     'name'=>'サイドバー',
     'id' => 'side_widget',
-    'before_widget'=>'<div id="%1$s" class="%2$s sidebar_wrapper">',
+    'description' => '通常のサイドバーです。',
+    'before_widget'=>'<div id="%1$s" class="widget %2$s">',
     'after_widget'=>'</div>',
     'before_title' => '<h4 class="sidebar_title">',
-    'after_title' => '</h4>'
+    'after_title' => '</h4>',
     ));
+
+  /****************
+  // フッター左
+  ****************/
+  register_sidebar(array(
+    'name' => 'フッターウィジェット（左）',
+    'id' => 'footer_left_widget',
+    'description' => 'フッターの左側に表示されるウィジェットです。真ん中と右のフッターウィジェットに何も配置されていない場合は中央寄せになります。',
+    'before_widget'=>'<div id="%1$s" class="widget %2$s">',
+    'after_widget'=>'</div>',
+    'before_title' => '<h4 class="sidebar_title">',
+    'after_title' => '</h4>',
+  ));
+
+  /****************
+  // フッター真ん中
+  ****************/
+  register_sidebar(array(
+    'name' => 'フッターウィジェット（真ん中）',
+    'id' => 'footer_center_widget',
+    'description' => 'フッターの真ん中に表示されるウィジェットです。左と右のフッターウィジェットに何も配置されていない場合は中央寄せになります。',
+    'before_widget'=>'<div id="%1$s" class="widget %2$s">',
+    'after_widget'=>'</div>',
+    'before_title' => '<h4 class="sidebar_title">',
+    'after_title' => '</h4>',
+  ));
+
+  /****************
+  // フッター右
+  ****************/
+  register_sidebar(array(
+    'name' => 'フッターウィジェット（左）',
+    'id' => 'footer_right_widget',
+    'description' => 'フッターの右側に表示されるウィジェットです。左と真ん中のフッターウィジェットに何も配置されていない場合は中央寄せになります。',
+    'before_widget'=>'<div id="%1$s" class="widget %2$s">',
+    'after_widget'=>'</div>',
+    'before_title' => '<h4 class="sidebar_title">',
+    'after_title' => '</h4>',
+  ));
+
 } //END register_widgets()
