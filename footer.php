98a7cd356a4a0f8ae6cbec865429d25e53f7b4a7
tkskds <infourbh+1@gmail.com>
2019-11-29 12:36:16 +0900

    fix: animation

--- footer.php
+++ footer.php
@@ -1,13 +1,25 @@
     <footer id="footer" class="footer">
       <div class="container footer_container">
         <?php
-          wp_nav_menu(array(
-            'theme_location' => 'nav_footer',
-            'container' => 'nav',
-            'container_id' => 'nav_footer',
-            'container_class' => 'nav_footer',
-            'fallback_cb' => ''
-          ));
+          if(
+            is_active_sidebar('footer_left_widget')||
+            is_active_sidebar('footer_center_widget')||
+            is_active_sidebar('footer_right_widget')) :
+        ?>
+          <div class="footer_widgets_wrap">
+            <?php if(is_active_sidebar('footer_left_widget')) dynamic_sidebar('footer_left_widget'); ?>
+            <?php if(is_active_sidebar('footer_center_widget')) dynamic_sidebar('footer_center_widget'); ?>
+            <?php if(is_active_sidebar('footer_right_widget')) dynamic_sidebar('footer_right_widget'); ?>
+          </div>
+        <?php endif; ?>
+        <?php
+          // wp_nav_menu(array(
+          //   'theme_location' => 'nav_footer',
+          //   'container' => 'nav',
+          //   'container_id' => 'nav_footer',
+          //   'container_class' => 'nav_footer',
+          //   'fallback_cb' => ''
+          // ));
         ?>
       </div>
     </footer>
