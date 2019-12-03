ddce4f662b79e2a17752ea78e496a8f245302260
tkskds <infourbh+1@gmail.com>
2019-12-03 17:19:06 +0900

    fix: pagination

--- lib/functions/for_setup/iii_customizer.php
+++ lib/functions/for_setup/iii_customizer.php
@@ -1349,7 +1349,7 @@ nav a.siteTitle{font-family:<?php echo $fontTitle ?>;font-size: <?php echo $titl
 色****************************
 ******************************
 *****************************/
-.main__color,.main_color:active,.main_color:focus,.main_color:hover,.main_color:visited,.main_color:focus-within{background:<?php echo $main_c ?>;}
+.main__color,.main_color:active,.main_color:focus,.main_color:hover,.main_color:visited,.main_color:focus-within,.pagination li span{background:<?php echo $main_c ?>;}
 .sub__color,.sub_color:active,.sub_color:focus,.sub_color:hover,.sub_color:visited,.sub_color:focus-within{background:<?php echo $sub_c ?>;}
 .acc__color,.acc_color:active,.acc_color:focus,.acc_color:hover,.acc_color:visited,.acc_color:focus-within{background:<?php echo $acc_c ?>;}
 /*その他個別に設定すべきメイン.サブ.アクセントカラー*/
