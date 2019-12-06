516e65f0a466e48bf936e28b22685c88bb584f18
tkskds <infourbh+1@gmail.com>
2019-12-07 08:58:53 +0900

    add: widgetStyle

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form method="get" action="<?php bloginfo('url'); ?>">
-  <div class="input-field search_form_wrap">
+  <div class="search_form_wrap">
     <input id="s" name="s" type="text" placeholder="検索キーワード" required>
     <button type="submit"><i class="material-icons">search</i></button>
   </div>
