4ce0552ec5f73b1fd24667dbed7ba42751b91cce
tkskds <infourbh+1@gmail.com>
2019-12-12 23:03:17 +0900

    add: defer

--- parts/footer/link_js.php
+++ parts/footer/link_js.php
@@ -4,10 +4,10 @@
 ?>
 
 <?php if ($cssfw != 'value1') : ?>
-  <script src="<?php echo get_template_directory_uri(); ?>/vendor/materialize/js/materialize.min.js"></script>
-  <script src="<?php echo get_template_directory_uri(); ?>/lib/js/materialize.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/vendor/materialize/js/materialize.min.js" defer></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/lib/js/materialize.js" defer></script>
 <?php endif; ?>
-<script src="<?php echo get_template_directory_uri(); ?>/vendor/delighters/min/delighters.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/vendor/delighters/min/delighters.min.js" defer></script>
 <?php if ($tocOn == true) : ?>
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/lib/js/toc.js" async></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/lib/js/toc.js" defer></script>
 <?php endif; ?>
