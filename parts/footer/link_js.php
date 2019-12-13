eab3ee6085ac7f8ba861e19fa3a2adb4fd69af0c
tkskds <infourbh+1@gmail.com>
2019-12-13 14:10:55 +0900

    fix: Dirtree

--- parts/footer/link_js.php
+++ parts/footer/link_js.php
@@ -7,7 +7,7 @@
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/vendor/materialize/js/materialize.min.js" defer></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/lib/js/materialize.js" defer></script>
 <?php endif; ?>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/vendor/delighters/min/delighters.min.js" defer></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/vendor/delighters/delighters.min.js" defer></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/vendor/lazysizes/lazysizes.min.js" async=""></script>
 <?php if ($tocOn == true) : ?>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/lib/js/toc.js" defer></script>
