5f21e9df707efe6e5a7275608b13ca7bd4f5f939
tkskds <infourbh+1@gmail.com>
2019-12-16 15:52:16 +0900

    fix: layout

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,19 @@
 <?php $d = get_option('site_widgets_design') ? get_option('site_widgets_design') : 'value1' ?>
 
-<aside id="aside" class="aside col l3<?php if($d == 'value1'){echo' sideType1' ;}elseif($d == 'value2'){echo' sideType2';}elseif($d=='value3'){echo' sideType3';} ?>">
+<aside id="aside" class="aside col l3
+<?php
+  if($d == 'value1'){
+    echo ' sideType1' ;
+  }elseif($d == 'value2'){
+    echo ' sideType2';
+  }elseif($d == 'value3'){
+    echo ' sideType3';
+  }
+  if (is_single()){
+    echo ' exsist_bread';
+  }
+?>
+">
   <div class="aside__container">
 
     <?php if(is_active_sidebar('side_widget')) :?>
